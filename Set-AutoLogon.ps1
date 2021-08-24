99bdf8befd3bff36f7df7e35d89ff8a6e670d2ef
Kasper Johansen <37331486+kaspersmjohansen@users.noreply.github.com>
2021-08-24 08:02:15 +0200

    Update Set-AutoLogon.ps1

--- Set-AutoLogon.ps1
+++ Set-AutoLogon.ps1
@@ -3,19 +3,19 @@
 **************************************************************************************************************************************
 Name:               Set-Autologon
 Author:             Kasper Johansen
-Website:            https://virtualwarlock.net            
+Company:            edgemo
+Contact:            kjo@edgemo.com
+Version:            1.0            
 Last modified by:   Kasper Johansen
-Last modified Date: 25-10-2020
+Last modified Date: 11-12-2019
 
 # Changes
 11-12-2019 - Added taskkill command, on line 113, to force Excel, Word and Outlook to forcefully close, before log off.
-28-12-2019 - Removed the above taskkill change. Added a dedicated job for killing processes before logoff. 
-             The job is now a part of the log off job on line 128.
+23-12-2019 - Removed the above taskkill change. Added a dedicated job for killing processes before logoff. 
+             The job is part of the ForEach loop starting on line 109.
              The job is implemented the make sure the auto logon user is properly logged off, 
              even when applications for some reason hangs or are waiting for input.
-02-03-2020 - Added support for executable arguments in the XML file. Look at the provided XML file for an example.
-03-03-2020 - Added support for execuatble working directory in the XML file. Look at the provided XML file for an example.
-25-10-2020 - Moved the script to it's own Github repo and removed the version information
+24-08-2021 - Fixed an issue with the randomly generated secure password not being configured correct in registry
 
 ******************************************************************************************************************************************
 
@@ -53,8 +53,8 @@ Last modified Date: 25-10-2020
     param(
          [Parameter(Mandatory = $true)]
          [string]$Username,
-         [Parameter(Mandatory = $true)]
-         [string]$Password,
+         #[Parameter(Mandatory = $true)]
+         #[string]$Password,
          [string]$AutoLogonXML = ".\AutoLogon.xml"
          )
 
@@ -66,7 +66,6 @@ function Create-ScheduledTask
          [Parameter(Mandatory = $true)]
          [string]$AppExecutable,
          [string]$AppArgument,
-         [string]$AppWorkingDir,
          [Parameter(Mandatory = $true)]
          [string]$User,
          [Parameter(Mandatory = $true)]
@@ -77,22 +76,10 @@ function Create-ScheduledTask
             {
                 $A = New-ScheduledTaskAction –Execute $AppExecutable -Argument $AppArgument
             }
-            
-            If (!([string]::IsNullOrWhiteSpace($AppWorkingDir)))
-            {
-                $A = New-ScheduledTaskAction –Execute $AppExecutable -WorkingDirectory $AppWorkingDir
-            }
-
-
-            If ((!([string]::IsNullOrWhiteSpace($AppArgument)) -and (!([string]::IsNullOrWhiteSpace($AppWorkingDir)))))
-            {
-                $A = New-ScheduledTaskAction –Execute $AppExecutable -Argument $AppArgument -WorkingDirectory $AppWorkingDir    
-            }
-            
-            If (([string]::IsNullOrWhiteSpace($AppArgument)) -and ([string]::IsNullOrWhiteSpace($AppWorkingDir)))
-            {
-                $A = New-ScheduledTaskAction –Execute $AppExecutable    
-            }
+            else
+                {
+                    $A = New-ScheduledTaskAction –Execute $AppExecutable    
+                }
 
             $T = New-ScheduledTaskTrigger -AtLogon -User $LogonUser
             $P = New-ScheduledTaskPrincipal -UserId $User -LogonType Interactive
@@ -101,40 +88,60 @@ function Create-ScheduledTask
             Register-ScheduledTask $TaskName -TaskPath \AutoLogon -InputObject $D    
     }
 
+function New-RandomPassword {
+    param(
+        [Parameter()]
+        [int]$MinimumPasswordLength = 5,
+        [Parameter()]
+        [int]$MaximumPasswordLength = 10,
+        [Parameter()]
+        [int]$NumberOfAlphaNumericCharacters = 5,
+        [Parameter()]
+        [switch]$ConvertToSecureString
+    )
+    
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
+    if ($ConvertToSecureString.IsPresent) {
+        ConvertTo-SecureString -String $password -AsPlainText -Force
+    } else {
+        $password
+    }
+}
+
 function Set-AutoLogon
     {
 
 # Create auto logon user
-$LocalUserPwd = $Password | ConvertTo-SecureString -AsPlainText -Force
+$LocalUserPwd = New-RandomPassword -MinimumPasswordLength 10 -MaximumPasswordLength 15 -NumberOfAlphaNumericCharacters 6 -ConvertToSecureString
 New-LocalUser -Name $Username -Password $LocalUserPwd -Verbose
 Add-LocalGroupMember -Group "Users" -Member $Username -Verbose
 
 # Configure auto logon user
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$SecurePasswordInMemory = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($LocalUserPwd)
+$Password = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($SecurePasswordInMemory)
 Set-ItemProperty -Path $RegPath -Name "AutoAdminLogon" -Value "1" -type String  
 Set-ItemProperty -Path $RegPath -Name "DefaultUsername" -Value "$Username" -type String  
 Set-ItemProperty -Path $RegPath -Name "DefaultPassword" -Value "$Password" -type String
 Set-ItemProperty -Path $RegPath -Name "AutoLogonCount" -Value "1" -type DWord
 
 # Create Shceduled Task to delete auto logon user password information
-Create-ScheduledTask -TaskName "Delete auto logon user password info" -User SYSTEM -LogonUser $Username -AppExecutable "%windir%\System32\reg.exe" -AppArgument "delete `"HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon`" /v DefaultPassword /f" -AppWorkingDir ""
+Create-ScheduledTask -TaskName "Delete auto logon user password info" -User SYSTEM -LogonUser $Username -AppExecutable "%windir%\System32\reg.exe" -AppArgument "delete `"HKLM\Software\Microsoft\Windows NT\CurrentVersion\Winlogon`" /v DefaultPassword /f"
 
 # Get apps in Autologon.xml
 [xml]$Configuration = Get-Content -Path .\AutoLogon.xml
 $Apps = $Configuration.SelectNodes("//Apps/App")
 $AppsPath = $Configuration.SelectNodes("//Apps/App").Path
 $AppsExecutable = $Configuration.SelectNodes("//Apps/App").Executable
-[string]$AppExecArgument = $Configuration.SelectNodes("//Apps/App").Argument
-[string]$AppExecWorkingDir = $Configuration.SelectNodes("//Apps/App").WorkingDir
 
 # Create scheduled task for each defined in Autologon.xml
 ForEach ($App in $Apps)
 {
     $AppPath = $App.Path
     $AppExec = $App.Executable
-    $AppArgu = $App.Argument
-    $AppWork = $App.WorkingDir
-    Create-ScheduledTask -TaskName $App.Name -User $Username -LogonUser $Username -AppExecutable "$AppPath\$AppExec" -AppArgument $AppArgu -AppWorkingDir $AppWork
+    Create-ScheduledTask -TaskName $App.Name -User $Username -LogonUser $Username -AppExecutable "$AppPath\$AppExec"
 }
 
 # Create scheduled task to kill processes before logoff
@@ -143,7 +150,13 @@ ForEach ($App in $AppsExecutable)
 [array]$KillApps +=$App -replace ".exe",""
 }
 $KillApps = $KillApps -join ","
-Create-ScheduledTask -TaskName "Log off auto logon user" -User $Username -LogonUser $Username -AppExecutable "%windir%\system32\WindowsPowerShell\v1.0\powershell.exe" -AppArgument "-WindowStyle Minimized -Command `"& {Start-Sleep -S 240; Stop-Process -Name $KillApps -Force; Start-Sleep -S 60; logoff}`"" -AppWorkingDir ""
+Create-ScheduledTask -TaskName "Log off auto logon user" -User $Username -LogonUser $Username -AppExecutable "%windir%\system32\WindowsPowerShell\v1.0\powershell.exe" -AppArgument "-WindowStyle Minimized -Command `"& {Start-Sleep -S 240; Stop-Process -Name $KillApps -Force; Start-Sleep -S 60; logoff}`""
+
+
+# Create scheduled task to logoff the auto logon user
+#Create-ScheduledTask -TaskName "Logoff auto logon user" -User $Username -LogonUser $Username -AppExecutable "%windir%\system32\cmd.exe" -AppArgument "/c `"timeout /t 300 && taskkill /IM excel.exe /F && taskkill /IM winword.exe /F && taskkill /IM outlook.exe /F && logoff`""
+#Create-ScheduledTask -TaskName "Logoff auto logon user" -User $Username -LogonUser $Username -AppExecutable "%windir%\system32\cmd.exe" -AppArgument "/c `"timeout /t 300 && logoff`""
+
     }
 
-Set-AutoLogon $Username $Password $AutoLogonXML
+Set-AutoLogon $Username $AutoLogonXML
